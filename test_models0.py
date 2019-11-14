60bacac22ea4532c65e1a750f26ba3f04ce09d83
JiazeWang <jiazewang@csu.edu.cn>
2019-11-14 19:59:37 +0800

    1

--- test_models0.py
+++ test_models0.py
@@ -55,48 +55,6 @@ args.train_list = "/home/jzwang/code/Video_3D/movienet/data/movie/movie_train.tx
 args.val_list = "/home/jzwang/code/Video_3D/movienet/data/movie/movie_val.txt"
 args.root_path = ""
 prefix = "frame_{:04d}.jpg"
-class AverageMeter(object):
-    """Computes and stores the average and current value"""
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
-
-
-def accuracy(output, target, topk=(1,)):
-    """Computes the precision@k for the specified values of k"""
-    maxk = max(topk)
-    batch_size = target.size(0)
-    _, pred = output.topk(maxk, 1, True, True)
-    pred = pred.t()
-    correct = pred.eq(target.view(1, -1).expand_as(pred))
-    res = []
-    for k in topk:
-         correct_k = correct[:k].view(-1).float().sum(0)
-         res.append(correct_k.mul_(100.0 / batch_size))
-    return res
-
-
-def parse_shift_option_from_log_name(log_name):
-    if 'shift' in log_name:
-        strings = log_name.split('_')
-        for i, s in enumerate(strings):
-            if 'shift' in s:
-                break
-        return True, int(strings[i].replace('shift', '')), strings[i + 1]
-    else:
-        return False, None, None
-
 
 weights_list = args.weights.split(',')
 test_segments_list = [int(s) for s in args.test_segments.split(',')]
